ba6e6ead238f196b5132d1d2b4c393d2e262ef8d
beyzaaltuntas <30345525+beyzaaltuntas@users.noreply.github.com>
2019-04-03 18:10:33 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='HiddenMarkovModel',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='beyza',
+    author_email='',
+    description=''
+)
